53f086a3ec9504cb88c3fe3a5ac6fdbe6d695445
jeanvicquintero-spec <jeanvicquintero@gmail.com>
2026-05-01 19:37:12 -0300

    Add CONTACT_INFO constant with company details

---
+++ src/constants/contact.ts
@@ -0,0 +1,36 @@
+export const CONTACT_INFO = {
+  // Información General
+  appName: 'CupomGanho',
+  
+  // Tu Email (cambiar por el tuyo)
+  email: 'Cupomganho@gmail.com',
+  
+  // Tu Teléfono/WhatsApp (cambiar por el tuyo)
+  phone: '+55 85986380362',
+  
+  // Redes Sociales
+  whatsapp: 'https://wa.me/5511987654321',
+  instagram: '@tuinstagram',
+  facebook: 'https://facebook.com/cupomganho',
+  
+  // Dirección (Oficina/Local - si tienes)
+  address: 'Ceará, CE - Brasil',
+  
+  // Horário de Atención
+  businessHours: 'Lun-Vie: 9:00 - 18:00',
+  
+  // URLs
+  website: 'https://www.cupomganho.com',
+  privacyPolicy: 'https://www.cupomganho.com/privacy',
+  termsAndConditions: 'https://www.cupomganho.com/terms',
+  
+  // Redes Sociales Alternativas
+  tiktok: '@cupomganho',
+  youtube: 'https://youtube.com/@cupomganho',
+  
+  // Información Bancaria (para Pix y transferencias)
+  pix: 'tuechave@pix.com',
+  
+  // Descripción de la empresa
+  description: 'CupomGanho - Economice y gane cashback en sus compras',
+};
